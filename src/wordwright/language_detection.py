c27cb04503eb1ad47a86ca2b3f02f6c7cf0a61b4
Yingzi Jin <jinyz8888@gmail.com>
2024-01-12 12:30:31 -0800

    add docstring for language_detection

---
+++ src/wordwright/language_detection.py
@@ -0,0 +1,30 @@
+import re
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from langdetect import detect
+from nltk.tokenize import sent_tokenize
+nltk.download('punkt')
+
+
+def language_detection(text):
+    """Detect if the text is in English or not.
+
+    Parameters
+    ----------
+    text : str
+        The text to be checked.
+
+    Returns
+    -------
+    str: "English" if the text is detected to be in English, "Not English" otherwise,
+         or "Language detection error" in case of an error.
+
+    Examples
+    --------
+    >>> language_detection("text")
+    """
+    pass
