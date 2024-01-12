ae59430388f222525560d6ec1385cbe76ce9c602
Yi Han <yi.han.178@gmail.com>
2024-01-12 12:27:24 -0800

    add text preprocessing functions

---
+++ src/wordwright/preprocessing.py
@@ -0,0 +1,40 @@
+def load_text(file_path):
+    """ Load and return the content of a text file. 
+    Removing punctuation, converting to lowercase.
+
+    Parameters
+    ----------    
+    file_path: str 
+        The path to the text file to be read.
+    
+    Returns
+    ---------- 
+    str
+        The content of the file as a string.
+
+    Examples
+    --------
+    >>> load_text("text.txt")
+    """
+    pass
+
+def clean_text(text):
+    """
+    Clean a text string by removing punctuation and converting to lowercase.
+
+    Parameters
+    ----------
+    text : str
+        The text to be cleaned.
+
+    Returns
+    ---------- 
+    str
+        The cleaned text.
+    
+    Examples
+    --------
+    >>> clean_text("Under the radiant moon, a silent owl glided over the tranquil lake.")
+    'under the radiant moon a silent owl glided over the tranquil lake'
+    """
+    pass
