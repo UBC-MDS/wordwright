b32c7b6655ae1dfcebea75e291496047f20a95b4
Yi Han <yi.han.178@gmail.com>
2024-01-15 19:28:11 -0800

    load_text and clean_text code & doc done.

--- src/wordwright/preprocessing.py
+++ src/wordwright/preprocessing.py
@@ -1,6 +1,11 @@
+from string import punctuation
+import os
+
 def load_text(file_path):
     """ Load and return the content of a text file. 
-    Removing punctuation, converting to lowercase.
+
+    This function reads a text file from the specified file path, but 
+    does not modify the content of the file.
 
     Parameters
     ----------    
@@ -11,16 +16,31 @@ def load_text(file_path):
     ---------- 
     str
         The content of the file as a string.
+    
+    Raises
+    ------
+    FileNotFoundError
+        If the file specified by file_path does not exist.
+    OSError
+        If there is an error opening or reading the file.
 
     Examples
     --------
     >>> load_text("text.txt")
     """
-    pass
+    try:
+        with open(file_path, "r") as file:
+            txt = file.read()
+        return txt
+    except FileNotFoundError as e:
+        print("Error 1: ", e)
+    except OSError as e:
+        print("Error 2: ", e)
 
 def clean_text(text):
     """
-    Clean a text string by removing punctuation and converting to lowercase.
+    Clean a text string by removing punctuation (except apostrophes), converting to 
+    lowercase, and removing excessive spaces and tabs.
 
     Parameters
     ----------
@@ -34,7 +54,19 @@ def clean_text(text):
     
     Examples
     --------
-    >>> clean_text("Under the radiant moon, a silent owl glided over the tranquil lake.")
-    'under the radiant moon a silent owl glided over the tranquil lake'
+    >>> clean_text("It's a sunny day. ,Let's GO!")
+    "it's a sunny day let's go"
     """
-    pass
+    if not isinstance(text, str):
+        raise TypeError("The input must be a string.")
+
+    modified_punctuation = punctuation.replace("'", "")
+
+    txt = text.lower()
+    for p in modified_punctuation:
+        txt = txt.replace(p, "")
+
+    # Split the text into words and join back together to remove excessive whitespace
+    txt = ' '.join(txt.split())
+
+    return txt
