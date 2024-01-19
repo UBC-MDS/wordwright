7e29f7fb7defb47896bcc8db589f89cb72bf819a
carrieyanyi <carrieyanyi@gmail.com>
2024-01-19 09:57:50 -0800

    update test function

--- tests/test_count_sentense.py
+++ tests/test_count_sentense.py
@@ -4,12 +4,12 @@ from wordwright import count_sentences
 def test_empty_string():
     text = ""
     punctuation = ["."]
-    assert count_sentences(text, punctuation) == 0
+    assert count_sentences(text, punctuation) == 0, "Sentenses counted incorrectly"
 
 def test_no_punctuation_input():
     text = "xcxccfssgjkvvhjmbh"
     punctuation = ["."]
-    assert count_sentences(text, punctuation) == 0
+    assert count_sentences(text, punctuation) == 0, "Sentenses counted incorrectly"
 
 def test_invalid_punctuation_input():
     text = "Hello world! It's a beautiful day? Yes, it is."
@@ -19,9 +19,9 @@ def test_invalid_punctuation_input():
 def test_multiple_sentence_endings():
     text = "Hello world! It's a beautiful day? Yes, it is."
     punctuation = [".", "?", "!"]
-    assert count_sentences(text, punctuation) == 3
+    assert count_sentences(text, punctuation) == 3, "Sentenses counted incorrectly"
 
 def test_multiple_occurrences(self):
     text = "Exciting!!! Really exciting!!! Isn't it?"
     punctuation = ["?", "!"]
-    assert count_sentences(text, punctuation) == 3
+    assert count_sentences(text, punctuation) == 3, "Sentenses counted incorrectly"
